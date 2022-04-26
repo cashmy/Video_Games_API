7e8e5a18dbcce4b68f2a53b06db1d52235ba8381
Cash Myers <cmyers880@gmail.com>
2022-04-25 20:49:14 -0500

    feat: Created endpoints GetPublishers and GetGamesByPublisher

--- ASP_NET_Video_Games_API/Controllers/GamesController.cs
+++ ASP_NET_Video_Games_API/Controllers/GamesController.cs
@@ -7,11 +7,11 @@ namespace ASP_NET_Video_Games_API.Controllers
     // api/examples
     [Route("api/[controller]")]
     [ApiController]
-    public class ExamplesController : ControllerBase
+    public class GamesController : ControllerBase
     {
         private readonly ApplicationDbContext _context;
 
-        public ExamplesController(ApplicationDbContext context)
+        public GamesController(ApplicationDbContext context)
         {
             _context = context;
         }
@@ -19,17 +19,22 @@ namespace ASP_NET_Video_Games_API.Controllers
         [HttpGet]
         public IActionResult GetPublishers()
         {
-            var videoGamePublishers = _context.VideoGames.Select(vg => vg.Publisher).Distinct();
-
+            var videoGamePublishers = _context.VideoGames
+                .Select(vg => vg.Publisher)
+                .Distinct();
             return Ok(videoGamePublishers);
         }
 
         [HttpGet("{pubName}")]
         public IActionResult GetGamesByPublisher(string pubName)
         {
-            var videoGames = _context.VideoGames.Where(vg => vg.Publisher == pubName);
+            int? maxYear = _context.VideoGames.Select(vg => vg.Year).Max();
+            int? minYear = _context.VideoGames.Select(vg => vg.Year).Min();
+
+            var videoGames = _context.VideoGames
+                .Where(vg => vg.Publisher == pubName);
+
             return Ok(videoGames);
         }
-
     }
 }
